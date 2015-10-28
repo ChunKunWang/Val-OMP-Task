327fdcdbeefcdb6d77bf8ec4f526a632f665c7f5
ChunKunWang <amos76530@gmail.com>
2015-10-28 11:29:20 -0400

    Create clean.sh

---
+++ clean.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Chun-Kun Wang (amos@cs.unc.edu)
+rm -rf a.out *.dot *.o *.ti temp_*.c temp_*.cpp rose_*.c rose_*.cpp amos_*.c amos_*.cpp amos_*.C amos_*.CPP
+rm -rf amos_tmp
+echo -e "Amos Clean OK!\a"
+
